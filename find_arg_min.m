b0af72166678b766c46a84bda78d573022ad758e
saharsamr <sahar.rajabi76@gmail.com>
2018-12-08 08:23:55 +0330

    delete extra prints.

--- find_arg_min.m
+++ find_arg_min.m
@@ -3,13 +3,10 @@ function [min_index]=find_arg_min(lower_bound, upper_bound)
     min_ = sum(range)^2;
     min_index = lower_bound;
     for i=lower_bound:upper_bound
-        i
         sigma = distance_sigma(i, lower_bound, upper_bound);
-        sigma
         if sigma < min_
             min_ = sigma;
             min_index = i;
-            min_index
         end
     end
 end
